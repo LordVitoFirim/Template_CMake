d0853875eca97f41abb0ac5ae31e78f281a43870
LordVitoFirim <kolfor63@gmail.com>
2026-04-29 22:01:20 +0300

    Create beginner CMake template variant

--- tests/test_math.cpp
+++ tests/test_math.cpp
@@ -1,13 +1,11 @@
-#include <cassert>
-
 #include "core/math_utils.hpp"
 
 int main()
 {
-    assert(core::add(2, 2) == 4);
-    assert(core::add(-2, 2) == 0);
-    assert(core::add(0, 0) == 0);
-    assert(core::add(-2, -3) == -5);
+    if (core::add(2, 2) != 4)
+    {
+        return 1;
+    }
 
     return 0;
 }
